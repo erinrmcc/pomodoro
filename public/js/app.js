f3b11efb289fdb07db2ae45a6f64967f6f4f4687
Erin McCaffrey <erinmccaffrey@Erins-MacBook-Pro.local>
2017-02-15 20:57:15 -0500

    adds pause function, accurately decreases minutes and seconds after start, ensures that appropriate two digit format is applied for minutes and seconds less than 10

--- public/js/app.js
+++ public/js/app.js
@@ -9,19 +9,52 @@ var numberOfBreaks = 0;
 var minutes = document.querySelector('#minutes');
 var seconds = document.querySelector('#seconds');
 var startButton = document.querySelector('#start');
+var pauseButton = document.querySelector('#pause');
 
 //Initialization code
 
 //Event listeners
 startButton.addEventListener('click', start);
+pauseButton.addEventListener('click', pause);
+render();
+
 
 //Function definitions
 function start(){
   if(!timer) {
-    timer = setInterval(tick, 1000)
+    timer = setInterval(tick, 1000);
   }
 }
-function tick(){
-  console.log('tick');
+function pause(){
+  timer = clearInterval(timer);
 }
-function render(){}
+function tick(){
+  decrementMinutes();
+  decrementSeconds();
+  render();
+}
+function decrementMinutes(){
+  if(secondsLeft === 0) {
+    minutesLeft -= 1;
+  }
+}
+function decrementSeconds(){
+  if(secondsLeft === 0){
+    secondsLeft = 59;
+  }
+  else {
+    secondsLeft -= 1;
+  }
+}
+function render(){
+  minutes.textContent = pad(minutesLeft);
+  seconds.textContent = pad(secondsLeft);
+}
+function pad(num){
+  if (num < 10){
+    return `0${num}`;
+  }
+  else {
+    return num;
+  }
+}
