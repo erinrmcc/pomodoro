476b563ed26864e1ceb4dc6c33432ff901f7e2e7
Erin McCaffrey <erinmccaffrey@Erins-MacBook-Pro.local>
2017-02-16 19:06:46 -0500

    resets to counter to include break times, counts the breaks to give different break times depending on cycle number, does not count below 0 and in fact stops the timer at 00

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 //Data and Variable declarations
 var timer;
-var minutesLeft = 25;
-var secondsLeft = 0;
+var minutesLeft = 0;
+var secondsLeft = 5;
 var isOnBreak = false;
 var numberOfBreaks = 0;
 
@@ -16,11 +16,12 @@ var pauseButton = document.querySelector('#pause');
 //Event listeners
 startButton.addEventListener('click', start);
 pauseButton.addEventListener('click', pause);
-render();
+render(); //pushes the initial time dated into the DOM
 
 
 //Function definitions
 function start(){
+  console.log(timer);
   if(!timer) {
     timer = setInterval(tick, 1000);
   }
@@ -29,6 +30,19 @@ function pause(){
   timer = clearInterval(timer);
 }
 function tick(){
+  if (secondsLeft === 0 && minutesLeft === 0){
+    clearInterval(timer);
+    timer = !timer; //dereference, sets back to undefined state
+    if(isOnBreak){
+      numberOfBreaks += 1;
+      resetWorkTime(); //on break is false, resets to 25
+    } else {
+      resetBreakTime(); //on break is true, resets to 5 to begin break
+    }
+    isOnBreak = !isOnBreak; //break switch - on/off
+    render();
+    return; //stops here so it doesn't increment and render again
+  }
   decrementMinutes();
   decrementSeconds();
   render();
@@ -41,8 +55,7 @@ function decrementMinutes(){
 function decrementSeconds(){
   if(secondsLeft === 0){
     secondsLeft = 59;
-  }
-  else {
+  } else {
     secondsLeft -= 1;
   }
 }
@@ -50,11 +63,23 @@ function render(){
   minutes.textContent = pad(minutesLeft);
   seconds.textContent = pad(secondsLeft);
 }
-function pad(num){
+function pad(num){ //sets 2 digit format - leading 0
   if (num < 10){
     return `0${num}`;
-  }
-  else {
+  } else {
     return num;
   }
+}
+function resetWorkTime(){
+  minutesLeft = 00;
+  secondsLeft = 05;
+}
+function resetBreakTime(){
+  if(numberOfBreaks < 3){
+    minutesLeft = 5;
+  } else {
+    minutesLeft = 15;
+    numberOfBreaks = 0; //resets the Pomodoro time for next 3 interations
+  }
+  secondsLeft = 0; //pulled out of both functions to minimize code
 }
